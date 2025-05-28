aa8c79cac26a4753da5f2f071c604fac0af27748
Naman <shahnaman755@gmail.com>
2025-05-28 17:09:59 +0530

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+# Create a sample dataframe 
+
+data = {'Name': ['Alice', 'Bob', 'Charlis'],
+        'Age': [25,30,35],
+        'City': ['New York', 'Los Angles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+
+
+# This is ensures that root direcctory exists at the root levels.
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+# Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# save the dataframe to a csv file, including column names
+df.to_csv(file_path, index=False)
+
+print(f"CSV file saved at: {file_path}")
